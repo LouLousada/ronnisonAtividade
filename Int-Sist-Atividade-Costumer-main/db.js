d3bb7126fdbaf120e779914b63f88530b4fe1d15
LouLousada <162380749+LouLousada@users.noreply.github.com>
2025-11-27 18:48:35 -0300

    Delete Int-Sist-Atividade-Costumer-main directory

--- Int-Sist-Atividade-Costumer-main/db.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv').config()
-const supabase = require('@supabase/supabase-js')
-
-const url = process.env.URL
-const key = process.env.KEY
-
-const cliente = supabase.createClient(url, key)
-
-module.exports = cliente
